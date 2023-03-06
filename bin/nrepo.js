3c06944582c6660ede52bfaef3a0decbccdcd11d
Melvin Carvalho <melvincarvalho@gmail.com>
2023-03-06 21:56:22 +0100

    add nrepo bin

---
+++ bin/nrepo.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+// IMPORTS
+import { generatePrivateKey } from 'nostr-tools'
+//const { generatePrivateKey } = nostrTools
+import {
+	getAllKeys
+} from 'noskey'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+// args
+const yarg = yargs(hideBin(process.argv))
+
+// console.log(yargs)
+const argv = yarg
+	.usage('Usage: $0 [options]')
+	.option('v', {
+		alias: 'vanity',
+		describe: 'Vanity string',
+		type: 'string'
+	})
+	.option('n', {
+		alias: 'npub',
+		describe: 'npub Vanity string',
+		type: 'string'
+	})
+	.option('p', {
+		alias: 'priv',
+		describe: 'Private key',
+		type: 'string'
+	})
+	.help('h')
+	.alias('h', 'help').argv
+
+// INIT
+// console.log(argv)
+var vanity = argv.v || ''
+var npubvanity = argv.n || ''
+
+
+
+// MAIN
+while (true) {
+	var privateKey = argv.p || generatePrivateKey()
+	var output = getAllKeys(privateKey)
+
+	const npub_prefix = 'npub1'
+	if (output.pubkey.startsWith(vanity) && output.npub.slice(npub_prefix.length).startsWith(npubvanity)) {
+                var ret = { npub: output.npub, nrepo: output.nrepo }
+
+		console.log(JSON.stringify(ret, null, 2))
+
+		break
+	}
+}
